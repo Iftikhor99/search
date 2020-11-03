3932d2e1d183d145f1cf08711bd7bd087085af0c
Iftikhor99 <1987-2005@mail.ru>
2020-11-03 23:28:29 +0500

    function All was rewritten

--- cmd/main.go
+++ cmd/main.go
@@ -1,32 +1,34 @@
 package main
 
 import (
-	"sync"
-	"strings"
-	"time"
-	"log"
 	"context"
+	"log"
+	"strings"
+	"sync"
+	"time"
 )
 
 //Result for
 type Result struct {
-	Prase string
-	Line string
+	Prase   string
+	Line    string
 	LineNum int64
-	ColNum int64
+	ColNum  int64
 }
 
 func main() {
 
-
-
 	//var files []string
-	file1 := "1;+992000000001;10000000;food\n2;+992000000002;20000000;auto\n3;+992000000003;30000000;auto"
+	root := context.Background()
+	file1 := "1;+992000000001;10000000;auto\n2;+992000000002;20000000;food\n3;+992000000003;30000000;auto"
 	file2 := "4;+992000000004;40000000;food\n5;+992000000005;50000000;food\n6;+992000000006;60000000;auto"
-	files :=[]string{file1, file2}
+	files := []string{file1, file2}
 
-	resultFound := all("food", files)
-	log.Print(resultFound)
+	for value := range all(root, "food", files) {
+		log.Print(value)	
+	}
+	// resultFound := all("food", files)
+	// log.Print(resultFound)
 	// done := make(chan struct{})
 
 	// go func (){
@@ -34,77 +36,83 @@ func main() {
 	// }()
 	// time.Sleep(time.Second*10)
 	// done <- struct{}{}
-	// ch := make(chan int)
-	// root := context.Background()
+	//	ch := make(chan int)
+
+	
 
 	log.Print("Cancel")
-	
 
-		
-	
 	//log.Print(ctx.Err())
-	//log.Print(ctx.Err() == context.Canceled) 
+	//log.Print(ctx.Err() == context.Canceled)
 	//log.Print(root.Err())
-	
+
 }
 
-func check(root context.Context) <- chan int {
+func check(root context.Context) <-chan int {
 	ch := make(chan int)
 
-	
 	ctx, cancel := context.WithCancel(root)
 	wg := sync.WaitGroup{}
 	wg.Add(1)
-	go func(ch chan int,ctx context.Context){
+	go func(ch chan int, ctx context.Context) {
 		for {
 			select {
-			case <- ctx.Done():
+			case <-ctx.Done():
 				log.Print("Done")
-			defer	wg.Done()
-					
-				
+				defer wg.Done()
+
 				return
-			case <- time.After(time.Second):
+			case <-time.After(time.Second):
 				log.Print("tick")
 				ch <- 1
-				
+
 			}
-			
-		}	
+
+		}
 	}(ch, ctx)
 
 	go func() {
-	close(ch)	
-	wg.Wait	()
+		close(ch)
+		wg.Wait()
 	}()
-	
+
 	cancel()
-	
 
 	log.Print("Cancel")
-	
+
 	return ch
 
 }
 
-func all(phrase string, files []string) []Result {
-	var resultTotal []Result
+func all(ctx context.Context, phrase string, files []string) <-chan []Result {
+	
 	var result Result
+	chanResult := make(chan []Result, len(files))
+	wg := sync.WaitGroup{}
 	for _, file := range files {
-		newData := strings.Split(file, "\n")
-		//log.Print(data)
-		//log.Print(newData)
+		var resultTotal []Result	
+		wg.Add(1)
+		go func(file string) {
+			newData := strings.Split(file,"\n")
+			//log.Print(data)
+			//log.Print(newData)
 
-		for i, stroka := range newData {
-			//log.Print(stroka)
-			if strings.Contains(stroka, phrase) == true {
-				result.Prase = phrase
-				result.Line = stroka
-				result.LineNum = int64(i+1)
-				result.ColNum = int64(strings.Index(stroka, phrase))
-				resultTotal = append(resultTotal, result)
+			for i, stroka := range newData {
+				//log.Print(stroka)
+				if strings.Contains(stroka, phrase) == true {
+					result.Prase = phrase
+					result.Line = stroka
+					result.LineNum = int64(i + 1)
+					result.ColNum = int64(strings.Index(stroka, phrase))
+					resultTotal = append(resultTotal, result)
+				}
+				
 			}
-			
-	}	}
-	return resultTotal
+			chanResult <- resultTotal
+			wg.Done()
+		}(file)
+	}
+	wg.Wait()
+	close(chanResult)
+	return chanResult
 }
--- pkg/search/search.go
+++ pkg/search/search.go
@@ -2,13 +2,11 @@ package search
 
 import (
 	"context"
-	"io/ioutil"
-	"log"
 	"strings"
 	"sync"
 )
 
-//Result ..
+//Result for
 type Result struct {
 	Phrase  string
 	Line    string
@@ -16,131 +14,36 @@ type Result struct {
 	ColNum  int64
 }
 
-//All ...
+//All for
 func All(ctx context.Context, phrase string, files []string) <-chan []Result {
-	ch := make(chan []Result)
+	
+	var result Result
+	chanResult := make(chan []Result, len(files))
 	wg := sync.WaitGroup{}
-
-	//var results []Result
-
-	ctx, cancel := context.WithCancel(ctx)
-
-	for i := 0; i < len(files); i++ {
+	for _, file := range files {
+		var resultTotal []Result	
 		wg.Add(1)
+		go func(file string) {
+			newData := strings.Split(file,"\n")
+			//log.Print(data)
+			//log.Print(newData)
 
-		go func(ctx context.Context, filename string, i int, ch chan<- []Result) {
-			defer wg.Done()
-
-			res := FindAllMatchTextInFile(phrase, filename)
-
-			if len(res) > 0 {
-				ch <- res
+			for i, stroka := range newData {
+				//log.Print(stroka)
+				if strings.Contains(stroka, phrase) == true {
+					result.Phrase = phrase
+					result.Line = stroka
+					result.LineNum = int64(i + 1)
+					result.ColNum = int64(strings.Index(stroka, phrase))
+					resultTotal = append(resultTotal, result)
+				}
+				
 			}
-
-		}(ctx, files[i], i, ch)
+			chanResult <- resultTotal
+			wg.Done()
+		}(file)
 	}
-
-	go func() {
-		defer close(ch)
-		wg.Wait()
-
-	}()
-
-	cancel()
-	return ch
-}
-
-//Any ...
-func Any(ctx context.Context, phrase string, files []string) <-chan Result {
-
-	ch := make(chan Result)
-	wg := sync.WaitGroup{}
-	result := Result{}
-	for i := 0; i < len(files); i++ {
-
-		data, err := ioutil.ReadFile(files[i])
-		if err != nil {
-			log.Println("error not opened file err => ", err)
-		}
-		filetext := string(data)
-
-		if strings.Contains(filetext, phrase) {
-			res := FindAnyMatchTextInFile(phrase, filetext)
-			if (Result{}) != res {
-				result = res
-				break
-			}
-		}
-
-	}
-	log.Println(result)
-
-	wg.Add(1)
-	go func(ctx context.Context, ch chan<- Result) {
-		defer wg.Done()
-		if (Result{}) != result {
-			ch <- result
-		} 
-	}(ctx, ch)
-
-	go func() {
-		defer close(ch)
-		wg.Wait()
-	}()
-	return ch
-}
-
-//FindAllMatchTextInFile ...
-func FindAllMatchTextInFile(phrase, fileName string) (res []Result) {
-
-	data, err := ioutil.ReadFile(fileName)
-	if err != nil {
-		log.Println("error not opened file err => ", err)
-		return res
-	}
-
-	file := string(data)
-
-	temp := strings.Split(file, "\n")
-
-	for i, line := range temp {
-		//fmt.Println("[", i+1, "]\t", line)
-		if strings.Contains(line, phrase) {
-
-			r := Result{
-				Phrase:  phrase,
-				Line:    line,
-				LineNum: int64(i + 1),
-				ColNum:  int64(strings.Index(line, phrase)) + 1,
-			}
-
-			res = append(res, r)
-		}
-	}
-
-	return res
-}
-
-//FindAnyMatchTextInFile ...
-func FindAnyMatchTextInFile(phrase, filetext string) (res Result) {
-
-	//ch := make(chan Result)
-
-	temp := strings.Split(filetext, "\n")
-
-	for i, line := range temp {
-		//fmt.Println("[", i+1, "]\t", line)
-		if strings.Contains(line, phrase) {
-
-			return Result{
-				Phrase:  phrase,
-				Line:    line,
-				LineNum: int64(i + 1),
-				ColNum:  int64(strings.Index(line, phrase)) + 1,
-			}
-
-		}
-	}
-
-	return res
+	wg.Wait()
+	close(chanResult)
+	return chanResult
 }
